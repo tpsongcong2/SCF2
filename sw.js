5da15f5b120e2a00502c04a17f226b123d527e9c
tpsongcong2 <tpsongcong2@gmail.com>
2026-06-12 12:06:40 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'scf-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png'];
+const CACHE = 'scf-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
